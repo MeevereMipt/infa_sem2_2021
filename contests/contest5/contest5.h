605be2f39d2d5e182872855f984bb55541f3f920
EGor <bike034@mail.ru>
2021-02-16 14:19:34 +0300

    made all needed .h files for contests
    (for future use)

---
+++ contests/contest5/contest5.h
@@ -0,0 +1,21 @@
+//
+// Created by EGor on 16.02.2021.
+//
+
+#ifndef MIPT2021_2SEM_CONTEST5_H
+#define MIPT2021_2SEM_CONTEST5_H
+
+namespace contest5 {
+    int taskA();
+    int taskB();
+    int taskC();
+    int taskD();
+    int taskE();
+    int taskF();
+    int taskG();
+    int taskH();
+
+    int task(char index);
+}
+
+#endif //MIPT2021_2SEM_CONTEST5_H
---
+++ contests/contest6/contest6.h
@@ -0,0 +1,21 @@
+//
+// Created by EGor on 16.02.2021.
+//
+
+#ifndef MIPT2021_2SEM_CONTEST6_H
+#define MIPT2021_2SEM_CONTEST6_H
+
+namespace contest6 {
+    int taskA();
+    int taskB();
+    int taskC();
+    int taskD();
+    int taskE();
+    int taskF();
+    int taskG();
+    int taskH();
+
+    int task(char index);
+}
+
+#endif //MIPT2021_2SEM_CONTEST6_H
