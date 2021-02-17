8b9627e5b7e2ce6221bb8b623eabb2efb12138a9
EGor <bike034@mail.ru>
2021-02-17 16:09:38 +0300

    something in taskA

--- contests/contest3/taskA.cpp
+++ contests/contest3/taskA.cpp
@@ -2,9 +2,31 @@
 // Created by EGor on 16.02.2021.
 //
 
+#include <iostream>
+
 namespace contest3 {
     int taskA(){
 
+
+        return 0;
+    }
+
+    int stupid_taskA(){
+        unsigned int array[255];
+
+        unsigned int N;
+        unsigned long sum = 0, out_sum = 0;
+
+        std::cin >> N;
+        for( int i=0; i<N; i++ ){
+            std::cin >> array[i];
+            sum += array[i];
+        }
+
+        for( int i=0; i<N; i++ ){
+            if( N*array[i] > sum )
+                out_sum += array[i]
+        }
 
         return 0;
     }
