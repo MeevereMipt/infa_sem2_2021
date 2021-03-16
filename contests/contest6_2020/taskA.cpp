a3c99321447ff73e9f3092c9074cc4908cd21848
EGor <bike034@mail.ru>
2021-03-16 13:36:26 +0300

    contest 6 (contest5_2020) two tasks (A and B)
    also changed cmake file
    to make it more convenient

---
+++ contests/contest6_2020/taskA.cpp
@@ -0,0 +1,35 @@
+//
+// Created by EGor on 16.03.2021.
+//
+
+#include <iostream>
+#include "my_stack.h"
+
+namespace contest6{
+
+    int taskA(){
+
+        int n, m, input;
+        std::cin >> n >> m;
+
+        Stack front, back;
+        for(int i=0; i<m; i++){
+            std::cin >> input;
+            front.push(input);
+        }
+
+        for(int i=m; i<n; i++){
+            std::cin >> input;
+            back.push(input);
+        }
+
+        while(!back.is_empty()){
+            std::cout << back.pop() << ' ';
+        }
+        while(!front.is_empty()){
+            std::cout << front.pop() << ' ';
+        }
+        std::cout << std::endl;
+        return 0;
+    }
+}
---
+++ contests/contest6_2020/taskB.cpp
@@ -0,0 +1,28 @@
+//
+// Created by EGor on 16.03.2021.
+//
+
+#include <iostream>
+#include "my_stack.h"
+
+namespace contest6{
+
+    int taskB(){
+        int n, input;
+        Stack stack;
+
+        std::cin >> n;
+
+        for(int i=0; i<n; i++){
+            std::cin >> input;
+            stack.push(input);
+        }
+
+        while(!stack.is_empty())
+            std::cout << stack.pop() << ' ';
+        std::cout << std::endl;
+
+        return 0;
+    }
+
+}
