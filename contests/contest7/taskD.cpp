cea3dc34649e4a10814ffe3f5af50662edf7af0d
EGor <bike034@mail.ru>
2021-03-23 14:57:59 +0300

    task D
    i guess it's better to use queue there
    but it's not implemented yet

--- contests/contest7/taskD.cpp
+++ contests/contest7/taskD.cpp
@@ -2,3 +2,42 @@
 // Created by EGor on 23.03.2021.
 //
 
+#include <iostream>
+#include "my_array.h"
+
+namespace contest7{
+
+#define KNIGHT 1;
+#define LIAR 0;
+
+    struct Entry{
+        std::string name;
+        bool type;
+
+        std::istream & operator >> (std::istream &in,  Entry& ent);
+        std::ostream & operator << (std::ostream &out, const Entry& ent);
+    };
+
+    std::istream &Entry::operator>>(std::istream &in, Entry &ent) {
+        in >> name >> type;
+        return in;
+    }
+
+    std::ostream &Entry::operator<<(std::ostream &out, const Entry &ent) {
+        out << name << ' ' << int(type);
+        return out;
+    }
+
+    int taskD(){
+        int n; bool news;
+        std::cin >> n >> news;
+
+        Array<Entry> people(n);
+        std::cin >> people;
+
+        int days;
+        std::cin >> days;
+
+        return 0;
+    }
+}
