3807feb5380237c7ba3c3b753de780196b652fb7
Egogorka <bike034@mail.ru>
2021-03-23 10:34:00 +0300

    contest 6 task E
    doesnt work - isnt the final cut

---
+++ contests/contest6_2020/taskE.cpp
@@ -0,0 +1,45 @@
+//
+// Created by Meevere on 3/23/2021.
+//
+
+#include <iostream>
+#include "my_array.h"
+
+namespace contest6 {
+
+    void swap( int& a, int& b ){
+        int tmp;
+        tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    void updated_bubble_sort(Array<int>& arr){
+        bool was_swapped = false;
+        int size = arr.get_size();
+        do {
+            was_swapped = false;
+            for(int i=0; i<size-1; i++){
+                if( arr[i] > arr[i+1] ) {
+                    swap(arr[i], arr[i + 1]);
+                    was_swapped = true;
+                }
+            }
+        } while (was_swapped);
+    }
+
+
+    int taskD() {
+
+        int n;
+        std::cin >> n;
+        Array<int> arr(n);
+
+        std::cin >> arr;
+        bubble_sort(arr);
+        std::cout << arr;
+
+        return 0;
+    }
+
+}
